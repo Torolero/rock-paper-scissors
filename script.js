5f4bbfb3c246f6a3bea120fdd38392a78b846aee
Torolero <42136673+Torolero@users.noreply.github.com>
2026-03-16 16:32:11 -0700

    Add new function to determine a winner
    Added function that evaluates both responses and
    determines the winner. It announces it and then
    adds points to the scoreboard.

--- script.js
+++ script.js
@@ -9,14 +9,19 @@ function getComputerChoice() {
 
     if (choice >= 0.66) {
         console.log("rock");
+        choice = "rock";
     } else if (choice <= 0.33) {
         console.log("paper");
+        choice = "paper";
     }else {
         console.log("scissors");
+        choice = "scissors";
     }
     return choice;
 }
-getComputerChoice();
+let computerChoice = getComputerChoice();
+
+
 
 /*
 User will now input a choice
@@ -26,6 +31,53 @@ The computer will return user choice
 
 function getHumanChoice() {
     let userChoice = prompt("Rock? Paper? Scissors?");
+    userChoice = userChoice.toLowerCase();
     return userChoice;
 }
-getHumanChoice();
+let userChoice = getHumanChoice();
+
+/*
+Create variables for computer and user score
+*/
+
+let humanScore = 0;
+let computerScore = 0;
+
+/*
+Take both the computer and user choices
+Compare both choices and determine winner
+Increment the winning player score by one
+Announce the winner
+*/
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats Rock");
+        computerScore++;
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats Scissors");
+        humanScore++;
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats Paper");
+        computerScore++;
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beats Paper");
+        humanScore++;
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats Scissors");
+        computerScore++;
+    } else if (humanChoice == computerChoice){
+        console.log("Nobody wins. It's a Tie!");
+    }
+
+}
+playRound(userChoice, computerChoice);
+
+/*
+Need to be able to play the game for 5 rounds
+Keep track of score across rounds
+Announces winner at the end
+*/
