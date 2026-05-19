c8be8314f1e9bbb9330cba8eb06cf989cce9b974
Torolero <42136673+Torolero@users.noreply.github.com>
2026-05-18 21:05:08 -0700

    Add UI to app
    This will add UI via buttons and text content to the
    rock-paper-scissors game as opposed to running on cli.
    This is a branch test.

--- script.js
+++ script.js
@@ -3,6 +3,55 @@ Plan: No user interface, just console.
 Randomly generate an input (rock, paper or scissors).
 Return input to the user as a string.
 */
+const body = document.querySelector("body");
+
+const rockButton = document.createElement("button");
+rockButton.textContent = "Rock";
+body.appendChild(rockButton);
+
+const paperButton = document.createElement("button");
+paperButton.textContent = "Paper";
+body.appendChild(paperButton);
+
+const scissorsButton = document.createElement("button");
+scissorsButton.textContent = "Scissors";
+body.appendChild(scissorsButton);
+
+let humanScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll("button");
+
+const mainDiv = document.createElement("div");
+body.appendChild(mainDiv);
+
+const scoresParagraph = document.createElement("p");
+
+const choicesP = document.createElement("p");
+
+const roundResults = document.createElement("p");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const humanChoice = button.textContent.toLowerCase();
+    const computerChoice = getComputerChoice();
+
+    playRound(humanChoice, computerChoice);
+
+    scoresParagraph.textContent = `Computer Score is: ${computerScore} and Human Score is: ${humanScore}`;
+    mainDiv.appendChild(scoresParagraph);
+
+    choicesP.textContent = `Computer Choice is: ${getComputerChoice()} and Human Choice is: ${button.textContent.toLocaleLowerCase()}`;
+    mainDiv.appendChild(choicesP);
+    mainDiv.appendChild(roundResults);
+
+    console.log("Computer Score is: " + computerScore);
+    console.log("Human Score is: " + humanScore);
+
+    winner();
+  });
+});
+
 
 function getComputerChoice() {
     let choice = Math.random();
@@ -18,9 +67,8 @@ function getComputerChoice() {
         choice = "scissors";
     }
     return choice;
-}
+};
 // let computerChoice = getComputerChoice();
-
 
 
 /*
@@ -29,19 +77,18 @@ The computer will take that choice
 The computer will return user choice
 */
 
-function getHumanChoice() {
+/*function getHumanChoice() {
     let humanChoice = prompt("Rock? Paper? Scissors?");
     humanChoice = humanChoice.toLowerCase();
     return humanChoice;
-}
+}*/
+
 // let humanChoice = getHumanChoice();
 
 /*
 Create variables for computer and user score
 */
 
-let humanScore = 0;
-let computerScore = 0;
 
 /*
 Take both the computer and user choices
@@ -50,32 +97,40 @@ Increment the winning player score by one
 Announce the winner
 */
 
+
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == "rock" && computerChoice == "paper") {
         console.log("You lose! Paper beats Rock");
+        roundResults.textContent = `You lose! Paper beats Rock`;
+
         computerScore++;
     } else if (humanChoice == "rock" && computerChoice == "scissors") {
         console.log("You win! Rock beats Scissors");
+        roundResults.textContent = "You win! Rock beats Scissors";
+
         humanScore++;
     } else if (humanChoice == "paper" && computerChoice == "scissors") {
         console.log("You lose! Scissors beats Paper");
+        roundResults.textContent = "You lose! Scissors beats Paper";
         computerScore++;
     } else if (humanChoice == "paper" && computerChoice == "rock") {
         console.log("You win! Paper beats Rock");
+        roundResults.textContent = "You win! Paper beats Rock";
         humanScore++;
     } else if (humanChoice == "scissors" && computerChoice == "paper") {
         console.log("You win! Scissors beats Paper");
+        roundResults.textContent = "You win! Scissors beats Paper";
         humanScore++;
     } else if (humanChoice == "scissors" && computerChoice == "rock") {
         console.log("You lose! Rock beats Scissors");
+        roundResults.textContent = "You lose! Rock beats Scissors";
         computerScore++;
     } else if (humanChoice == "rock" && computerChoice == "rock" || humanChoice == "paper" && computerChoice == "paper" || humanChoice == "scissors" && computerChoice == "scissors"){
         console.log("Nobody wins. It's a Tie!");
+        roundResults.textContent = "Nobody wins. It's a Tie!";
     }
-    return computerScore;
-    return humanScore;
-
 }
+
 // playRound(humanChoice, computerChoice);
 
 
@@ -101,7 +156,7 @@ function playGame() {
     return humanScore;
     return computerScore;
 }
-playGame();
+// playGame();
 
 // for (let i = 0; i < 4; i++) {
 //     playGame();
@@ -115,5 +170,11 @@ function winner() {
 } else {
     console.log("It's a tie!");
 }
+
+    if (humanScore == 5) {
+        alert("Human Wins!");
+    } else if (computerScore == 5) {
+        alert("Computer Wins!");
+    }
 }
-winner();
+
